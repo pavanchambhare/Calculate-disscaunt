349218ccc174f3992ce02f30aae6b624fcce7830
pavanchambhare <pavanchambhare88@gmail.com>
2022-05-19 14:17:52 +0530

    first commit

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
- let num1 = 8
- let num2 = 5
-
- console.log(num1* num2)
-
- let answer = num1 > num2
-
- console.log(answer)
-
-
-//  a +( (b * C) /d) * 100
-// D =(L -S)/L *100
-
-let sellingPrice = 199;
-let listPrice = 799;
- 
-
-let discontPercent = ((listPrice - sellingPrice) /listPrice * 100)
-console.log("Desscount Percent: "  +  discontPercent)
-
-displayDiscoutPercentage = Math.round(discontPercent)
-
-console.log(displayDiscoutPercentage + "% off")
-
-
-let result = listPrice > sellingPrice
-console.log(typeof result)
+let Menu = document.getElementById(".menu")
+let container = document.querySelector('.container')
+menu.onclick = function(){
+    container.classList.toggle('active')
+}
